43fc8b1a6b76a48bea448e05cc4f09398aca70de
SurlyDuck <santoslucas347@gmail.com>
2026-08-20 16:18:48 -0300

    Tile editor grid

--- editor/src/level.c
+++ editor/src/level.c
@@ -1,5 +1,7 @@
 #include "editor.h"
 
+#define CAMERA_SPEED 900
+
 const char *gridValues[] = {
 	"16x16",
 	"32x32",
@@ -19,15 +21,19 @@ const char *directories[] = {
 
 // Locals
 editor_state_id nextState;
-static uint16_t gridWidth         = 16;
-static uint16_t gridHeight        = 16;
-static uint16_t tileWidth         = 128;
-static uint16_t tileHeight        = 64;
+static uint16_t gridWidth      = 16; // Maybe move grid variables into a `world` structure?
+static uint16_t gridHeight     = 16;
+static uint16_t tileWidth      = 128;
+static uint16_t tileHeight     = 64; 
+static uint16_t textureWidth;
+static uint16_t textureHeight;
 static SDL_Texture *gridTargetTexture  = NULL;
 static SDL_FRect gridScreenRect = {0};
 static SDL_FRect gridTargetRect = {0};
 
 // Local foward declarations
+static void DrawTile(uint16_t x, uint16_t y, uint8_t w, uint8_t h);
+static void DrawGrid();
 static void _OnButtonBack();
 static void _OnButtonGridSize();
 static void _OnButtonLoadTileset();
@@ -35,7 +41,6 @@ static void _OnChangeGridSize(const char *newVal);
 
 void ExitLevel();
 editor_state_id UpdateLevel(SDL_Renderer *renderer);
-
 
 void InitLevel(editor_state *state){
 	state->Update  = UpdateLevel;
@@ -58,30 +63,70 @@ void InitLevel(editor_state *state){
 	gridScreenRect.h = WINDOW_HEIGHT - 200;
 	gridScreenRect.y = 200;
 	
-	int textureWidth = gridWidth * tileWidth;
-	int textureHeight = gridHeight * tileHeight;
+	textureWidth = gridWidth * tileWidth + tileWidth * 2;
+	textureHeight = gridHeight * tileHeight + tileHeight * 2;
 	gridTargetTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, textureWidth, textureHeight);
 
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_SetRenderTarget(renderer, gridTargetTexture); // Starts rendering the grid
-	SDL_RenderRect(renderer, &(SDL_FRect){0,0,100,100});
+	DrawGrid();
 	SDL_SetRenderTarget(renderer, NULL); // Stops rendering the grid
+
+	// Essentialy this is the camera 
+	gridTargetRect.x = textureWidth/2 - gridTargetRect.w/2;
 }
 
-float y = 0;
 editor_state_id UpdateLevel(SDL_Renderer *renderer){
+	SDL_Event *event = GetInputEvents();
+	for(; event->type != 0; ++event){
+		switch(event->type){
+			case SDL_EVENT_KEY_DOWN :{
+				SDL_FRect cpyTemp = gridTargetRect;
+				gridTargetRect.y += ((event->key.key == SDLK_K) - (event->key.key == SDLK_J)) * CAMERA_SPEED * 16/1000;
+				gridTargetRect.x += ((event->key.key == SDLK_L) - (event->key.key == SDLK_H)) * CAMERA_SPEED * 16/1000;
+				
+				if(gridTargetRect.x+gridTargetRect.w >= textureWidth || gridTargetRect.x < 0) gridTargetRect.x = cpyTemp.x;
+				if(gridTargetRect.y+gridTargetRect.y >= textureHeight || gridTargetRect.y < 0) gridTargetRect.y = cpyTemp.y;
+				break;
+			}
+
+		}
+
+	}
+
 	// Screen margins
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_RenderRect(renderer, &gridScreenRect);
 	
 	SDL_RenderTexture(renderer, gridTargetTexture, &gridTargetRect, &gridScreenRect);
-	
-	y +=.01;
-	gridTargetRect.y += y;
-	gridTargetRect.x += y;
+
 
 	UpdateGuiElements();
 	return nextState;
+}
+
+static void DrawTile(uint16_t x, uint16_t y, uint8_t w, uint8_t h){
+	SDL_RenderLine(renderer, w/2-1 + x, h-1 + y, x, h/2 + y);
+	SDL_RenderLine(renderer, w/2 + x, h-1 + y, w-1 + x, h/2 + y);
+	SDL_RenderLine(renderer, w/2-1 + x, y, x, h/2 + y);
+	SDL_RenderLine(renderer, w/2 + x, y, w-1 + x, h/2 + y);
+
+}
+
+static void DrawGrid(){
+	//DrawTile(textureWidth/2 - tileWidth/2, tileHeight, 128, 64);
+	
+	uint16_t tileStartX = textureWidth/2 - tileWidth/2;
+	uint16_t tileStartY = tileHeight;
+	for(uint16_t x = 0; x < gridWidth; ++x){
+		for(uint16_t y = 0; y < gridHeight; ++y){
+			uint16_t px = (x-y) * (tileWidth/2) + tileStartX;
+			uint16_t py = (x+y) * (tileHeight/2) + tileStartY;
+
+			DrawTile(px, py, tileWidth, tileHeight);
+		}
+		
+	}
 }
 
 static void _OnChangeGridSize(const char *newVal){
